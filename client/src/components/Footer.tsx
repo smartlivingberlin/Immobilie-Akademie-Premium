637e1096a94e9451ce703f504c22e35a89a13a29
Alischad <alisadgadyri38@gmail.com>
2026-03-15 14:45:03 +0100

    fix: schliessenden </Link> Tag bei Widerrufsbelehrung im Footer ergaenzt

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -70,7 +70,7 @@ export default function Footer() {
               <li>
                 <Link href="/datenschutz" className="hover:text-white transition-colors">
                   Datenschutzerklärung</Link>
-                <Link href="/widerruf" className="hover:text-white transition-colors">Widerrufsbelehrung
+                <Link href="/widerruf" className="hover:text-white transition-colors">Widerrufsbelehrung</Link>
                 </Link>
               </li>
               <li>
