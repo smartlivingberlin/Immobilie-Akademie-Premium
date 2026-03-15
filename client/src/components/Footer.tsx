fdeef5929ab2f9b6441996dd7cec6f1ee9face6f
Alischad <alisadgadyri38@gmail.com>
2026-03-15 16:37:02 +0100

    fix: leeres li im Footer entfernt

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -72,8 +72,6 @@ export default function Footer() {
                   Datenschutzerklärung</Link>
               </li>
               <li>
-              </li>
-              <li>
                 <Link href="/widerruf" className="hover:text-white transition-colors">Widerrufsbelehrung</Link>
               </li>
               <li>
