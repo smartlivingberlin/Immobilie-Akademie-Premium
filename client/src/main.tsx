06c4f0c1168288dbe01b0644378bc46cf9a8bb8a
Alisad Gadyri <alisadgadyri38@gmail.com>
2026-05-16 14:53:38 +0200

    fix: correct fonts.css import in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-./fonts.css
+import './fonts.css';
 import * as Sentry from "@sentry/react";
 if (import.meta.env.VITE_SENTRY_DSN) {
   Sentry.init({ dsn: import.meta.env.VITE_SENTRY_DSN, tracesSampleRate: 0.1 });
